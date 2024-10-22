8cb66d207ec5e2cd9b8220dd6bae99cea78f7686
armanimozart <zamo1932@yahoo.com>
2024-10-22 13:26:11 -0700

    Update after testing

--- src/com/coderscampus/assignment2/Main.java
+++ src/com/coderscampus/assignment2/Main.java
@@ -6,15 +6,14 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
-				
+	
 			int x = 5; 
 			int randomValue = new Random().nextInt(100);
 			int luckyNumber = randomValue + 1;
-
 			Scanner scanner = new Scanner(System.in);
 				
-			while (x >= 0) {
+			while (x >= 1) {
+				
 				System.out.println("Pick a number between 1 and 100");
 				String userInput = scanner.nextLine();
 				int guess = Integer.parseInt(userInput);
@@ -22,10 +21,10 @@ public class Main {
 				if (guess == luckyNumber) {
 					System.out.println("You win!");
 					break;
-				} else if (x == 0) {
+				} else if (x == 1) {
 					System.out.println("You lose. The number to guess was: " + luckyNumber);
 					break;
-				}else if (guess < 1 || guess > 100) {
+				} else if (guess < 1 || guess > 100) {
 					System.out.println("Your guess is not between 1 and 100, please try again");
 				} else if (guess < luckyNumber) {
 					System.out.println("Pick a higher number");
