834c1560659da6002ec171450d5cf466ec7e5b76
armanimozart <zamo1932@yahoo.com>
2024-10-23 22:19:24 -0700

    Refactored code

--- src/com/coderscampus/assignment2/Main.java
+++ src/com/coderscampus/assignment2/Main.java
@@ -6,33 +6,35 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	
-			int x = 5; 
-			int randomValue = new Random().nextInt(100);
-			int luckyNumber = randomValue + 1;
-			Scanner scanner = new Scanner(System.in);
-				
-			while (x >= 1) {
-				
-				System.out.println("Pick a number between 1 and 100");
-				String userInput = scanner.nextLine();
-				int guess = Integer.parseInt(userInput);
-					
-				if (guess == luckyNumber) {
-					System.out.println("You win!");
-					break;
-				} else if (x == 1) {
-					System.out.println("You lose. The number to guess was: " + luckyNumber);
-					break;
-				} else if (guess < 1 || guess > 100) {
-					System.out.println("Your guess is not between 1 and 100, please try again");
-				} else if (guess < luckyNumber) {
-					System.out.println("Pick a higher number");
-					x--;
-				} else if (guess > luckyNumber) {
-					System.out.println("Pick a lower number");
-					x--;
-				}
-			} 	scanner.close();
+		// Instantiate all variables
+		int guesses = 1; 
+		int maxNumberOfGuesses = 5;
+		int userGuess;
+		int luckyNumber = new Random().nextInt(100) + 1;
+		Scanner scanner = new Scanner(System.in);
+			
+		// Un-comment line below to view the number to guess
+		// System.out.println("The lucky number is: " + luckyNumber);
+		System.out.println("Pick a number between 1 and 100");
+			
+		// Logic
+		while (guesses <= maxNumberOfGuesses) {				
+			userGuess = Integer.parseInt(scanner.nextLine());				
+			if (userGuess < 1 || userGuess > 100) {
+				System.out.println("Your guess is not between 1 and 100, please try again");
+			} else if (userGuess == luckyNumber) {
+				System.out.println("You win!");
+				break;						
+			} else if (guesses == 5)	{
+				System.out.println("You lose. The number to guess was: " + luckyNumber);
+			} else if (userGuess < luckyNumber) {
+				System.out.println("Pick a higher number");
+				guesses++;
+			} else {
+				System.out.println("Pick a lower number");
+				guesses++;
+			}
 		}
+		scanner.close();
 	}
+}
